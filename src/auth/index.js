e2749dfe2c95753e6cd248579e839af0c4b9878f
leonardo gomes <leo291195@gmail.com>
2020-08-11 23:12:39 -0300

    signout complete

--- src/auth/index.js
+++ src/auth/index.js
@@ -52,3 +52,13 @@ export const signin = user =>{
         }).catch(err => console.log(err))
     }
   }
+
+  export const isAuthenticated=() =>{
+      if(typeof window == 'undefined'){
+        return false
+      }
+      if(localStorage.getItem('jwt')){
+          return JSON.parse(localStorage.getItem('jwt'))
+      }
+      else{ return false}
+  }
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Link, withRouter } from 'react-router-dom'
-import {signout} from '../auth'
+import {signout, isAuthenticated} from '../auth'
 import Signup from '../user/Signup'
 
 const isActive =(history, path)=>{
@@ -17,22 +17,28 @@ const Menu = ({history})=>(
             <li className='nav-item'>
                 <Link className='nav-link' style={isActive(history, '/')} to='/'>Home</Link>
             </li>
-            <li className='nav-item'>
-                <Link className='nav-link' 
-                style={isActive(history, '/signin')} 
-                to='/signin'>Signin</Link>
-            </li>
-            <li className='nav-item'>
-                <Link className='nav-link' style={isActive(history, '/signup')} to='/signup'>Signup</Link>
-            </li>
-            <li className='nav-item'>
-                <span className='nav-link' 
-                style={{cursor: 'pointer', color: '#ffffff'}} 
-                onClick={()=> signout( () => {
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className='nav-item'>
+                        <Link className='nav-link' 
+                         style={isActive(history, '/signin')} 
+                        to='/signin'>Signin</Link>
+                     </li>
+                    <li className='nav-item'>
+                        <Link className='nav-link' style={isActive(history, '/signup')} to='/signup'>Signup</Link>
+                    </li>
+                </Fragment>
+            )}
+            {isAuthenticated() && (
+                <li className='nav-item'>
+                    <span className='nav-link' 
+                    style={{cursor: 'pointer', color: '#ffffff'}} 
+                    onClick={()=> signout( () => {
                     history.push('/')
-                })
+                    })
                 }>Signout</span>
-            </li>
+                </li>
+            )}
         </ul>
     </div>
 )
